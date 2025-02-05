e7076062c1dd052d9cf7ecb093276ac12f0808ae
Albert Pregonas <albertpregonas@gmail.com>
2025-02-05 17:51:55 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const generateDomains = () => {
+    let domains = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    for (let a of adj) {
+      for (let p of pronoun) {
+        for (let n of noun) {
+          const domain = `${p}${a}${n}.com`;
+          domains.push(domain);
+        }
+      }
+    }
+
+    const domainList = document.getElementById("domainList");
+    domainList.innerHTML = "";
+
+    domains.forEach(domain => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("text-dark", "mb-2");
+      listItem.textContent = domain;
+      domainList.appendChild(listItem);
+    });
+  };
+
+  generateDomains();
+});
